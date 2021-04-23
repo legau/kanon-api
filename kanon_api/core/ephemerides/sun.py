67eb39ff043b8d9e5977f5657a88a56bc602cd7c
Léni Gauffier <lenigauffier@gmail.com>
2021-04-23 12:46:31 +0000

    refactor: tables in enum form

--- kanon_api/core/ephemerides/sun.py
+++ kanon_api/core/ephemerides/sun.py
@@ -1,46 +1,31 @@
-from typing import cast
-
-from astropy.units import degree
-from astropy.units.core import Unit
-from astropy.units.quantity import Quantity
-from kanon.calendars import Date
-from kanon.tables.symmetries import Symmetry
 from kanon.units import Sexagesimal
+from kanon.units.radices import BasedQuantity
+from .tables import FIXED_STARS, SUN
+from .constants import SUN_ROOT
+from kanon.calendars import Date
 
-from .utils import get_days, mean_motion, read_dishas
-
-degree = cast(Unit, degree)
-
-mm_mean_sun = mean_motion(193, Sexagesimal("4,38;21,0,30,28"))
-mm_fixed_stars = mean_motion(236, Sexagesimal(0))
-mm_access_recess = mean_motion(237, Sexagesimal("5,59;12,34"), zodiac_offset=2)
-
-tab_eq_access_recess = read_dishas(238)
-tab_eq_access_recess.symmetry = [
-    Symmetry("mirror"),
-    Symmetry("periodic", targets=[Sexagesimal(3, 1)]),
-]
-tab_eq_sun = read_dishas(19)
-tab_eq_sun.symmetry = [Symmetry("mirror", sign=-1)]
+from .utils import get_days, mod360
+from ...units import degree
 
 
-def sun_true_pos(date: Date) -> Quantity:
+SUN_APOGEE: BasedQuantity = Sexagesimal("1,11;25,23") * degree
+
+
+def sun_true_pos(date: Date) -> BasedQuantity:
     sexa_date = get_days(date)
 
-    mean_sun_pos = mm_mean_sun(sexa_date)
-    mean_fixed_star_pos = mm_fixed_stars(sexa_date)
-    access_recess_pos = mm_access_recess(sexa_date)
+    mean_sun_pos = SUN.mean_motion(sexa_date)
+    mean_fixed_star_pos = FIXED_STARS.mean_motion(sexa_date)
+    access_recess_pos = FIXED_STARS.access_recess_mm(sexa_date)
 
-    eq_access_recess = tab_eq_access_recess.get(access_recess_pos.value)
+    eq_access_recess = FIXED_STARS.access_recess_eq(access_recess_pos.value)
 
-    solar_apogee_pos = (
-        mean_fixed_star_pos + eq_access_recess + Sexagesimal("1,11;25,23") * degree
-    )
+    solar_apogee_pos: BasedQuantity = mean_fixed_star_pos + eq_access_recess + SUN_ROOT
 
-    mean_arg_sun = (mean_sun_pos - solar_apogee_pos) % (Sexagesimal(6, 0) * degree)
+    mean_arg_sun = mod360(mean_sun_pos - solar_apogee_pos)
 
-    eq_sun = tab_eq_sun.get(mean_arg_sun.value)
+    eq_sun = SUN.equation(mean_arg_sun.value)
 
-    true_pos_sun = (mean_sun_pos - eq_sun) % (Sexagesimal(6, 0) * degree)
+    true_pos_sun = mod360(mean_sun_pos - eq_sun)
 
     return true_pos_sun
