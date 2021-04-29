d8edf528ae27e017517fc682631b1fb020e010ab
Léni Gauffier <lenigauffier@gmail.com>
2021-04-29 18:36:29 +0000

    correct port

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 count=0
-until [[ $(curl -f -s http://localhost/health) ]] || [[ $count -ge 20 ]];
+until [[ $(curl -f -s http://localhost:8000/health) ]] || [[ $count -ge 20 ]];
 do
 	sleep 1
 	((count++))
 done
-curl -f http://localhost/health
+curl -f http://localhost:8000/health
