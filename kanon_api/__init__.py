e03d94053527d2ca780a46dd0bd3c1a455f852e4
Léni Gauffier <lenigauffier@gmail.com>
2021-05-28 10:43:19 +0000

    fix

--- kanon_api/__init__.py
+++ kanon_api/__init__.py
@@ -6,19 +6,14 @@ from kanon.tables import HTable
 from starlette import responses
 
 _to_pandas = HTable.to_pandas
-_init = HTable.__init__
 
 
 def to_pandas(self: HTable, *args, **kwargs):
-    return self.cached_to_pandas
+    if not hasattr(self, "cached_to_pandas"):
+        self.cached_to_pandas = _to_pandas(self)
+    return self.cached_to_pandas.copy()
 
 
-def init(self: HTable, *args, **kwargs):
-    _init(self, *args, **kwargs)
-    self.cached_to_pandas = _to_pandas(self)
-
-
-HTable.__init__ = init  # type: ignore
 HTable.to_pandas = to_pandas  # type: ignore
 
 responses.quote_plus = quote
