79f4971d72fa01273bbea8eaff45c267de9c1c37
Léni <lenigauffier@gmail.com>
2021-07-13 13:32:45 +0200

    feat: ascendant with fraction of days (#47)

--- kanon_api/core/ephemerides/ascendant.py
+++ kanon_api/core/ephemerides/ascendant.py
@@ -6,13 +6,15 @@ from .utils import mod
 
 
 def ascendant(days: float, latitude: float) -> BasedQuantity:
-    sun = sun_true_pos(days)
+    sun = sun_true_pos(round(days))
+
+    hours_arc = (days - round(days)) * 24 * 15 * sun.unit
 
     sun_rising = ObliqueAscension.get(sun, latitude)
     sun_setting = ObliqueAscension.get(mod(sun.value + 180), latitude)
 
     diurnal_arc = mod(sun_setting - sun_rising)
 
-    ascension_degree = mod(sun_rising + diurnal_arc / 2)
+    ascension_degree = mod(sun_rising + hours_arc + diurnal_arc / 2)
 
     return ObliqueAscension.reverse_get(ascension_degree, latitude)
